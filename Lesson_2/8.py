f61b1057d56322ca3416fc68cd25c8cf6215b2a5
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-12 22:51:13 +0300

    Решение задачи 08

--- Lesson_2/8.py
+++ Lesson_2/8.py
@@ -3,3 +3,27 @@
  последовательности чисел. Количество вводимых чисел и цифра,
  которую необходимо посчитать, задаются вводом с клавиатуры.
 """
+
+def count_digit(number, digit):
+      count = 0
+      for char in number:
+            if char == digit:
+                  count += 1
+      return count
+
+print('              -= Задание 8 =-')
+print(' Программа считает сколько раз встречается определенная цифра в введенной последовательности чисел.\n')
+
+count_number = int(input('Количество вводимых чисел: '))
+digit = int(input('Искомая цифра: '))
+
+quantity_digits = 0
+
+for i in range(1, count_number + 1):
+      number = input(f'Введите {i}-е число: ')
+      quantity = count_digit(number, str(digit))
+      print(f'В числе {number} содержится {quantity} цифр(ы) {digit}')
+      quantity_digits += quantity
+
+print(f'В введенных числах содержится {quantity_digits} цифр(ы) {digit}')
+
