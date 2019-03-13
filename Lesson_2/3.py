226dcec8d25829c582852d4720cbe8c61f106a1a
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-13 13:57:40 +0300

    Добавлена реализация рекурсии задачи 03 и 04

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -3,6 +3,17 @@
 цифр и вывести на экран. Например, если введено число 3486,
  то надо вывести число 6843.
 """
+<<<<<<< Updated upstream
+=======
+def number_reverse(num):
+	if num == 0:
+		return ''
+	else:
+		digits = num % 10
+		num = num // 10
+		return str(digits) + number_reverse(num)
+
+>>>>>>> Stashed changes
 
 print('              -= Задание 3 =-')
 print('Программа формирует из введенного числа обратное по порядку входящих в него цифр\n'
@@ -11,7 +22,17 @@ number = int(input('Введите натуральное число: '))
 a = number
 reverse_number = ''
 while a != 0:
+<<<<<<< Updated upstream
     digit = a % 10
     a = a // 10
     reverse_number = reverse_number + str(digit)
 print(f'{number} <===> {int(reverse_number)}')
+=======
+	digit = a % 10
+	a = a // 10
+	reverse_number = reverse_number + str(digit)
+print(f'{number} <===> {int(reverse_number)}')
+
+print('\n              Рекурсия')
+print(f'{number} <===> {int(number_reverse(number))}')
+>>>>>>> Stashed changes
