b2f5cc69c0161074d19e0c92370b40e6854b8123
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-14 23:16:35 +0300

    Решение задачи 08

--- Lesson_3/8.py
+++ Lesson_3/8.py
@@ -4,3 +4,25 @@
 записывать ее в последнюю ячейку строки.
 В конце следует вывести полученную матрицу.
 """
+
+print('                  -= Задание 8 =-')
+print('Программа обработки двумерного массива.\n')
+
+row = 5
+col = 4
+array = []
+for i in range(row):
+    arr_string = []
+    summa = 0
+    for j in range(col):
+        n = int(input(f'Введите {j + 1} элемент {i + 1} строки: '))
+        summa += n
+        arr_string.append(n)
+    arr_string.append(summa)
+    array.append(arr_string)
+
+print('Сформированный массив:\n')
+for i in range(row):
+    for j in range(row):
+        print(f'{array[i][j]: 5}', end='')
+    print()
