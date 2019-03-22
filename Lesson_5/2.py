7f0a242ca45721d0604b08741c52eb7861a1689a
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-22 21:13:52 +0300

    Решение задачи 02

--- Lesson_5/2.py
+++ Lesson_5/2.py
@@ -5,3 +5,26 @@
 [‘C’, ‘4’, ‘F’] соответственно. Сумма чисел из примера: [‘C’, ‘F’, ‘1’],
 произведение - [‘7’, ‘C’, ‘9’, ‘F’, ‘E’].
 """
+
+
+class HexNumber:
+    def __init__(self, c=''):
+        self.list = list(c)
+        self.mod = c
+
+    def __add__(self, obj):
+        self.sum = list(hex(int(self.mod, 16) + int(obj.mod, 16))[2:])
+
+    def __mul__(self, obj):
+        self.multy = list(hex(int(self.mod, 16) * int(obj.mod, 16))[2:])
+
+
+x = input('Введите шестнадцатиричное число А: ')
+y = input('Введите шестнадцатиричное число В: ')
+a = HexNumber(x)
+b = HexNumber(y)
+a + b
+a * b
+
+print(f'{a.list} + {b.list} = {a.sum}')
+print(f'{a.list} * {b.list} = {a.multy}')
