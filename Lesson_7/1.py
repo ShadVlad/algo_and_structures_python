3807cd5f425325fb500a3f5531841a55bcd2fcc3
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-30 10:35:15 +0300

    Задача 01

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,31 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+import timeit
+import random
+
+
+def bubble_sort(source):
+    n = 1
+    while n < len(source):
+        change = 0
+        for i in range(len(source)-n):
+            if source[i] > source[i+1]:
+                source[i],source[i+1] = source[i+1],source[i]
+                change += 1
+        if change == 0:
+            break
+        n += 1
+    return source
+
+
+# замеры
+source_list = [random.randint(-100, 100) for _ in range(25)]
+print('Исходный массив')
+print(source_list)
+print('Отсортированный массив')
+print(bubble_sort(source_list))
+
+print(timeit.timeit("bubble_sort(source_list)", \
+    setup="from __main__ import bubble_sort, source_list", number=10))
+
