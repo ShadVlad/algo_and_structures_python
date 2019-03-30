59afeea2f51a2abedabc5b3b7a587c401d15d014
ShadVlad <wowchik.shadrin@yandex.ru>
2019-03-30 10:36:22 +0300

    Задача 02

--- Lesson_7/2.py
+++ Lesson_7/2.py
@@ -3,3 +3,51 @@
 заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
 и отсортированный массивы.
 """
+import timeit
+import random
+
+def merge_sort(source):
+    if len(source) > 1:
+        center = len(source) // 2
+        left = source[:center]
+        right = source[center:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i, j, k = 0, 0, 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                source[k] = left[i]
+                i += 1
+            else:
+                source[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            source[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            source[k] = right[j]
+            j += 1
+            k += 1
+        return source
+
+
+
+# замеры
+source_list = [random.randint(0, 50) for _ in range(25)]
+
+print('Исходный массив')
+print(source_list)
+print('Отсортированный массив')
+print(merge_sort(source_list))
+
+print(timeit.timeit("merge_sort(source_list)", \
+    setup="from __main__ import merge_sort, source_list", number=10))
+
+
